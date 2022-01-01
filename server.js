169ff546d48c4abd95e4ec8fe0d26d392c7bd962
Moez B <moezbajwa@hotmail.com>
2022-01-01 08:17:45 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const SERVER_PORT = process.env.PORT || 3001;
 
 let app = express();
+
+//ware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
 //routes
-app.get('/', (req, res) => {
-    res.send('synchro');
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 //startup
